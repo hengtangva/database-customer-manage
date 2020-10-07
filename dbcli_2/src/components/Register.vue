bc5f0e5ac6385717e368913b11b6616b9d0226c7
facturi <facturi@163.com>
2020-10-07 22:44:08 +0800

    修改投诉界面

--- dbcli_2/src/components/Register.vue
+++ dbcli_2/src/components/Register.vue
@@ -1,20 +1,25 @@
 <template>
   <el-form ref="form" :model="Form" label-width="80px" class="bodyreg">
 
-      <el-form-item label="用户id">
-        <el-input v-model="Form.userId" disabled>
-        </el-input>
-      </el-form-item>
-
       <el-form-item label="用户名">
-        <el-input v-model="Form.username " disabled>
+        <el-input v-model="Form.username ">
         </el-input>
       </el-form-item>
 
-      <el-form-item label="投诉内容">
-        <el-input v-model="Form.complain" disabled>
+    <el-form-item label="联系电话">
+      <el-input v-model="Form.telephone">
+      </el-input>
+    </el-form-item>
+
+      <el-form-item label="投诉标题">
+        <el-input v-model="Form.complainTitle">
         </el-input>
       </el-form-item>
+
+    <el-form-item label="投诉内容">
+      <el-input type="textarea" autosize v-model="Form.complain">
+      </el-input>
+    </el-form-item>
 
     <el-button type="primary" @click="onSubmit">提交<i class="el-icon-upload el-icon--right"></i></el-button>
 
@@ -27,24 +32,19 @@
     data() {
       return {
         Form: {
-          username: 'th',
-          userId: '',
-          complain:'',
-          answer: '',
+          username: '',
+          telephone:'',
+          complainTitle:'',
+          complain:''
         }
       }
     },
     methods: {
      async onSubmit() {
-        const {data: res} = await this.$http.post('url',data);//发送请求，更新
-        if(res.meta.status !== 200) {
-          this.$message({
-            type: 'error',
-            message: '提交失败',
-            showClose: true
-          })
-          return
-        }
+        await this.$http.post(
+            'a.general',{type:"submit_complain",info:this.Form}
+        );//发送请求，更新
+
        this.$message({
          type: 'success',
          message: '提交成功',
